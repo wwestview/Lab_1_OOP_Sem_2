ef1088dd863189a1b404fc05d2f7e244987632fa
wwestview <biokapt@gmail.com>
2025-03-20 12:32:22 +0200

    Lab_1

---
+++ Laba_1/Laba_1/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Reflection.Emit;
+using System.Threading;
+using System.Threading.Channels;
+public class Timer
+{
+    private  int interval;
+    private  Action action;
+    private bool isRunning;
+    private Thread timeThread;
+    public Timer(int interval, Action action)
+    {
+        this.interval = interval * 1000;
+        this.action = action;
+    }
+    public void Start()
+    {
+        isRunning = true;
+        timeThread = new Thread(() =>
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(interval);
+                action.Invoke();
+            }
+        });
+        timeThread.Start();
+       
+    }
+    public void Stop () => isRunning = false;
+}
+class Program
+{
+    static void Main()
+    {
+        Action action = () => Console.WriteLine("Action executed at: " + DateTime.Now);
+        
+        Timer timer = new Timer(1, action);
+        Timer timer2 = new Timer(1, action = ()=> Console.WriteLine("Action executed at 2: " + DateTime.Now));
+        timer.Start(); timer2.Start();
+        Thread.Sleep(10000);
+        timer.Stop();
+        timer2.Stop();
+    }
+}
---
+++ Laba_1/Task_2/ArrayFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+using System;
+
+delegate bool Filter(int number);
+class Filters
+{
+    static void Main()
+    {
+        Console.Write("Enter your array: ");
+        int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+        Console.Write("k: ");
+        int k = int.Parse(Console.ReadLine());
+        Filter cond = number => number % k == 0;
+        int[] filtredWhere = array.Where(number =>cond(number)).ToArray();
+        Console.WriteLine("Filtred with 'Where': " + string.Join(",", filtredWhere));
+        int[] filtredManually = FilterArray(array, cond);
+        Console.WriteLine("Filtred manually: " + string.Join(",", filtredManually));
+    }
+   static int[] FilterArray(int[] array, Filter cond)
+    {
+        int count = 0;
+        foreach (var i in array)
+        {
+            if(cond(i)) count++;
+        }
+        int[] result =new int[count];
+        int indx = 0;
+        foreach (var i in array)
+        {
+            if (cond(i)) result[indx++] = i;
+        }
+        return result;
+    }
+   
+}
